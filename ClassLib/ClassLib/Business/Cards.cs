a68e5eda9c9874f551a93f5a695c25d9436fd680
Emre Ilbay <12201630@student.pxl.be>
2023-04-26 22:09:26 +0200

    Initial commit

---
+++ ClassLib/ClassLib/Business/Cards.cs
@@ -0,0 +1,27 @@
+﻿using ClassLib.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Business
+{
+    public class Cards
+    {
+        public Card PokemonCard()
+        {
+            return new Card
+            {
+                Name = "Pikachu",
+                HP = 60,
+                Type = "Electric",
+                Attack1 = "Thundershock",
+                Attack2 = "Thunderbolt",
+                Weakness = "Fighting",
+                Resistance = "Metal",
+                Generation = 1
+            };
+        }
+    }
+}
---
+++ ClassLib/ClassLib/Business/Entities/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Business.Entities
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public string Type { get; set; }
+        public string Attack1 { get; set; }
+        public string Attack2 { get; set; }
+        public string Weakness { get; set; }
+        public string Resistance { get; set; }
+        public int Generation { get; set; }
+    }
+}
